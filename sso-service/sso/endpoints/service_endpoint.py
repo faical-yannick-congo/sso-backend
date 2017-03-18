1362b6e7a693807d10d20fbf001b848098a7b70e
Faical Yannick Palingwende Congo <yannick.congo@gmail.com>
2017-03-17 22:48:05 -0400

    Fixing json import.

--- sso-service/sso/endpoints/service_endpoint.py
+++ sso-service/sso/endpoints/service_endpoint.py
@@ -1,4 +1,4 @@
-import json
+import simplejson as json
 
 from flask.ext.api import status
 import flask as fk
@@ -8,7 +8,6 @@ from sso import app, SERVICE_URL, service_response
 from ssodb.common.models import User, Service
 
 import mimetypes
-import json
 import traceback
 import datetime
 import random
